e20454a9fb0148d48c79b464d4c3d2005116ab92
Raihanarfi12 <onlyraihan00@gmail.com>
2024-11-02 20:04:54 +0700

    First commit for terraform challenge 1

---
+++ kodecloud/challenge-1/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+  }
+
+  required_version = "= 1.1.5"
+}
+
+provider "kubernetes" {
+  config_path = "/root/.kube/config"
+}
